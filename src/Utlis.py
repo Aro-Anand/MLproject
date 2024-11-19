943d753b1815460cf2355452e6f4eb21e667d9f3
Arockia Anand Raj D <aroanand21f@gmail.com>
2024-11-19 23:09:28 +0530

    Hyperparameter Tuning

--- src/Utlis.py
+++ src/Utlis.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill # type: ignore
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.Exception import CustomException
 from src.Logger import logging
@@ -24,13 +25,19 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test, y_test,models):
+def evaluate_models(X_train,y_train,X_test, y_test,models, param):
 
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train)         #Train the Model
 
